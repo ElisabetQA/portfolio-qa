dd0e10570106f95638dc560171382808e5b8cf53
Elisabet Gacía <elisabet.gar@gmail.com>
2026-03-25 18:33:01 +0000

    Update insert.sql

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,1 +1,10 @@
+INSERT INTO formportfolio (nombre, email, edad, motivo, mensaje)
+VALUES
+('Ana', 'ana@gmail.com', null, 'information', 'hola testing'),
+(null, 'pedro@gmail.com', 20, 'oferta de trabajo', 'pedro mensaje'),
+('Maria', 'maria@yahoo.com', 25, 'otros', 'mensaje de prueba'),
+('Ana', 'ana.work@gmail.com', 30, 'oferta de trabajo', 'test message'),
+(null, 'user1@gmail.com', 22, 'information', 'mensaje testing'),
+(Pedro', 'pedro@test.com', 35, 'otros', 'test mensaje largo'),
+('Juan', 'juan@gmail.com', null, 'oferta de trabajo', 'hola qa testing');
 
